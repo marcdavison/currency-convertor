e1d2efb7d057b38073c1d13f67b124e9c844f677
marcdavison <marcdavison77@hotmail.com>
2026-08-31 11:44:50 +0100

    create files

---
+++ src/app/pages/currency-convertor/currency-convertor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-convertor',
+  imports: [],
+  templateUrl: './currency-convertor.html',
+  styleUrl: './currency-convertor.scss',
+})
+export class CurrencyConvertor {
+
+}
