eec282b8e3306b8d9d843cda6042ec60dcd94954
marcdavison <marcdavison77@hotmail.com>
2026-08-31 11:51:59 +0100

    add css for structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CurrencyConvertor } from './pages/currency-convertor/currency-convertor';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {   path: "**", component: CurrencyConvertor    }
+];
